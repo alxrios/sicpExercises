aee783ab2a813a0f983d1057d7b2e38297268d09
Alejandro Ríos <76874671+alxrios@users.noreply.github.com>
2023-03-07 16:47:41 +0100

    feat: add exercise 1.3

--- sicpExercises1.scm
+++ sicpExercises1.scm
@@ -13,3 +13,32 @@ Translate the following expression into prefix form:
 
 (/ (+ 5 4 (- 2 (- 3 (+ 6 0.8))))
    (* 3 (- 6 2) (- 2 7)))
+
+#| Exercise 1.3:
+Define a procedure that takes three numbers as arguments and
+returns the sum of the squares of the two larger numbers.
+|#
+
+
+(define (exer13 x y z)
+  (cond ((and (or (> x y) (= x y)) (or (> y z) (= y z))) (+ (* x x) (* y y)))
+        ((and (or (> x y) (= x y)) (or (< y z) (= y z))) (+ (* x x) (* z z)))
+        ((and (or (< x y) (= x y)) (or (< x z) (= x z))) (+ (* y y) (* z z)))
+        ((and (or (< x y) (= x y)) (or (> x z) (= x z))) (+ (* y y) (* x x)))))
+
+
+;# Test:
+#|
+(exer13 5 7 9)
+(exer13 5 9 7)
+(exer13 7 5 9)
+(exer13 7 9 5)
+(exer13 9 5 7)
+(exer13 9 7 5)
+(exer13 7 7 5)
+(exer13 5 7 7)
+(exer13 7 5 7)
+(exer13 7 7 7)
+|#
+
+
